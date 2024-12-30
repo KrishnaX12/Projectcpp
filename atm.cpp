71e0c390b973a1259f7fe1a74b797f9d5914394a
Coder-X-12 <bkr.2007@yahoo.com>
2024-12-30 14:07:12 +0530

    modified atm.cpp

--- atm.cpp
+++ atm.cpp
@@ -1,34 +1,6 @@
-// #include<iostream>
-// using namespace std;
-// bool prime(int a)
-// {
-//     int c;
-//     for(int i=1;i<=a;i++)
-//     {
-//         if(i%a==0)
-//         {
-//             c++;
-//         }
-//     }
-//     if(c==2)
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-// }
-// int main()
-// {
-//     int a;
-//     cin>>a;
-//     if(prime(a))
-//     {
-//         cout<<"true";
-//     }
-//     else
-//     {
-//         cout<<"false";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"*******************WELCOME TO THE BANK****************************";
+}
