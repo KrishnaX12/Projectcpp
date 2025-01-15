4f0d7897063fd0b3ef17d02f3679ed5e6080dabb
Coder-X-12 <bkr.2007@yahoo.com>
2025-01-15 22:42:21 +0530

    changed

--- atm.cpp
+++ atm.cpp
@@ -1,6 +1,13 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    cout<<"*******************WELCOME TO THE BANK****************************";
+int main(){
+        vector<vector<int>>& matrix;
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i]==0){
+                (matrix.begin(),matrix.end())=0;
+            }
+        }
+        for(auto it : matrix){
+            cout<<*it<<" ";
+        }
 }
